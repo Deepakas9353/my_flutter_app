4c8c7c7864da8f6edee80402d6446f3009b72f35
Deepak A S <deepaks.9141@gmail.com>
2026-09-10 13:09:06 +0530

    latest web move 2026-sep-07-v1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"5a2a6a42cce67f965cf540fcecf616faca624a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2005018720" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3458717619" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
